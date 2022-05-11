14672d530d2c308f09e67b0f4914dadebbdce28f
Sofia Gromovaya <sonya.gromovaya2004@gmail.com>
2022-05-11 06:55:44 +0300

    again

---
+++ 01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+console.log(galleryItems);
+
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.innerHTML = markup;
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}">
+`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", keyboardClose);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", keyboardClose);
+      },
+    }
+  );
+  function keyboardClose(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+  instance.show();
+  // const visible = basicLightbox.visible();
+});
+
+console.log(galleryItems);
---
+++ 02-lightbox.js
@@ -0,0 +1,50 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+  )
+  .join("");
+
+gallery.innerHTML = markup;
+
+let instance = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+// gallery.addEventListener("click", (event) => {
+// event.preventDefault();
+// if (event.target.nodeName !== "IMG") return;
+
+//   console.log(event.target.alt);
+//   if (event.target.nodeName !== "IMG") return;
+
+//   const instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.source}">
+// `);
+//   instance.show();
+//   const visible = basicLightbox.visible();
+//   if (visible) {
+//     document.addEventListener("keydown", (event) => {
+//       if (event.code === "Escape") {
+//         instance.close();
+//       }
+//     });
+//   }
+//   var lightbox = new SimpleLightbox(".gallery a", {
+//     /* options */
+//     captionsData: "alt",
+//     captionPosition: "bottom",
+//     captionDelay: 250,
+//   });
+// });
